fc04e0328ceb4cb8a5dfdc7583dbedc384cc6c50
Marco <228495880+donmarco3@users.noreply.github.com>
2026-05-21 10:09:55 +0930

    exercise 19 - couldn't complete as webpage is payvaulted

---
+++ 19_decode_a_web_page_two.py
@@ -0,0 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://www.vanityfair.com/society/2014/06/monica-lewinsky-humiliation-culture'
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'html.parser')
