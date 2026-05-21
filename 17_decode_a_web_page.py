eed53bc9743c83a75418bba44fb6b55ad339dbac
Marco <228495880+donmarco3@users.noreply.github.com>
2026-05-21 09:36:07 +0930

    exercise 17 - not complete as nytimes changed their html structure

---
+++ 17_decode_a_web_page.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.nytimes.com/'
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'html.parser')
+
+for heading in soup.find_all(class_='css-5mgoji'):
+    print(heading)
